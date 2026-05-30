f1831ccb79b6daa9bf2e61c6183f32feb001c71f
prakash <prakashsaha1999@gmail.com>
2026-05-30 14:30:17 +0530

    chore: configure S3 upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,25 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+        region: env('AWS_REGION'),
+        endpoint: env('AWS_ENDPOINT'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
 
 export default config;
